c0b5536814fbc8fc94727950ac42052d4dbed599
aimov35 <93774344+aimov35@users.noreply.github.com>
2025-08-22 00:32:41 +0300

    x

--- api/index.js
+++ api/index.js
@@ -4,10 +4,18 @@ const app = express();
 app.set("view engine", "twig");
 app.use(express.static(__dirname + '/public'));
 
+
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
+app.listen(3000, () => {
+  console.log("Sunucu 3000 portunda çalışıyor");
+});
 
 module.exports = app;
